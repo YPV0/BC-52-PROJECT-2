6c41d99135ec33f4d053062f3ead6b3533344801
YPV0 <yuriisfw@gmail.com>
2023-05-05 23:39:34 +0300

    performance-upd

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,82 +1,95 @@
-import anime from 'animejs/lib/anime.es.js';
+// import anime from 'animejs/lib/anime.es.js';
 
-const container = document.querySelector('.tags-list');
-const tags = document.querySelectorAll('.tags-item');
+// const container = document.querySelector('.tags-list');
+// const tags = document.querySelectorAll('.tags-item');
 
-function moveTags() {
-  tags.forEach(tag => {
-    const dx = (Math.random() - 0.5) * 2 * 50;
-    const dy = (Math.random() - 0.5) * 2 * 50;
-    const rotation = (Math.random() - 0.5) * 2 * 360;
+// const tagData = Array.from(tags).map(tag => {
+//   const x = tag.offsetLeft + tag.clientWidth / 2;
+//   const y = tag.offsetTop + tag.clientHeight / 2;
+//   return {
+//     element: tag,
+//     x,
+//     y,
+//     maxX: container.clientWidth - tag.clientWidth,
+//     maxY: container.clientHeight - tag.clientHeight,
+//     rotation: 0,
+//   };
+// });
 
-    const tagX = tag.offsetLeft + tag.clientWidth / 2;
-    const tagY = tag.offsetTop + tag.clientHeight / 2;
+// function moveTags() {
+//   tagData.forEach(tag => {
+//     const dx = (Math.random() - 0.5) * 2 * 110;
+//     const dy = (Math.random() - 0.5) * 2 * 110;
+//     const rotation = (Math.random() - 0.5) * 2 * 360;
 
-    const x = tagX + dx;
-    const y = tagY + dy;
+//     const x = tag.x + dx;
+//     const y = tag.y + dy;
 
-    const maxX = container.clientWidth - tag.clientWidth;
-    const maxY = container.clientHeight - tag.clientHeight;
+//     const constrainedX = Math.min(
+//       Math.max(x, tag.element.clientWidth / 2),
+//       tag.maxX + tag.element.clientWidth / 2
+//     );
+//     const constrainedY = Math.min(
+//       Math.max(y, tag.element.clientHeight / 2),
+//       tag.maxY + tag.element.clientHeight / 2
+//     );
 
-    const constrainedX = Math.min(
-      Math.max(x, tag.clientWidth / 2),
-      maxX + tag.clientWidth / 2
-    );
-    const constrainedY = Math.min(
-      Math.max(y, tag.clientHeight / 2),
-      maxY + tag.clientHeight / 2
-    );
+//     tag.x = constrainedX;
+//     tag.y = constrainedY;
+//     tag.rotation = rotation;
 
-    anime({
-      targets: tag,
-      translateX: constrainedX - tagX,
-      translateY: constrainedY - tagY,
-      rotate: rotation,
-      duration: 3000,
-      easing: 'linear',
-      complete: () => {
-        moveTag(tag); // Call moveTag again for the same tag
-      },
-    });
-  });
-}
+//     anime({
+//       targets: tag.element,
+//       translateX:
+//         constrainedX - tag.element.offsetLeft - tag.element.clientWidth / 2,
+//       translateY:
+//         constrainedY - tag.element.offsetTop - tag.element.clientHeight / 2,
+//       rotate: rotation,
+//       duration: 3000,
+//       easing: 'linear',
+//       complete: () => {
+//         moveTag(tag); // Call moveTag again for the same tag
+//       },
+//     });
+//   });
+// }
 
-function moveTag(tag) {
-  const dx = (Math.random() - 0.5) * 2 * 50;
-  const dy = (Math.random() - 0.5) * 2 * 50;
-  const rotation = (Math.random() - 0.5) * 2 * 360;
+// function moveTag(tag) {
+//   const dx = (Math.random() - 0.5) * 2 * 110;
+//   const dy = (Math.random() - 0.5) * 2 * 110;
+//   const rotation = (Math.random() - 0.5) * 2 * 360;
 
-  const tagX = tag.offsetLeft + tag.clientWidth / 2;
-  const tagY = tag.offsetTop + tag.clientHeight / 2;
+//   const x = tag.x + dx;
+//   const y = tag.y + dy;
 
-  const x = tagX + dx;
-  const y = tagY + dy;
+//   const constrainedX = Math.min(
+//     Math.max(x, tag.element.clientWidth / 2),
+//     tag.maxX + tag.element.clientWidth / 2
+//   );
+//   const constrainedY = Math.min(
+//     Math.max(y, tag.element.clientHeight / 2),
+//     tag.maxY + tag.element.clientHeight / 2
+//   );
 
-  const maxX = container.clientWidth - tag.clientWidth;
-  const maxY = container.clientHeight - tag.clientHeight;
+//   tag.x = constrainedX;
+//   tag.y = constrainedY;
+//   tag.rotation = rotation;
 
-  const constrainedX = Math.min(
-    Math.max(x, tag.clientWidth / 2),
-    maxX + tag.clientWidth / 2
-  );
-  const constrainedY = Math.min(
-    Math.max(y, tag.clientHeight / 2),
-    maxY + tag.clientHeight / 2
-  );
+//   anime({
+//     targets: tag.element,
+//     translateX:
+//       constrainedX - tag.element.offsetLeft - tag.element.clientWidth / 2,
+//     translateY:
+//       constrainedY - tag.element.offsetTop - tag.element.clientHeight / 2,
+//     rotate: rotation,
+//     duration: 20000,
+//     easing: 'linear',
+//     complete: () => {
+//       moveTag(tag);
+//     },
+//   });
+// }
 
-  anime({
-    targets: tag,
-    translateX: constrainedX - tagX,
-    translateY: constrainedY - tagY,
-    rotate: rotation,
-    duration: 50000,
-    easing: 'linear',
-    complete: () => {
-      moveTag(tag);
-    },
-  });
-}
-
-tags.forEach(tag => {
-  moveTag(tag);
-});
+// tagData.forEach(tag => {
+//   moveTag(tag);
+// });
