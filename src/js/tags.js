3c385189ca2dca3d92249a5d666779b713b3ca94
YPV0 <yuriisfw@gmail.com>
2023-05-05 22:48:16 +0300

    update

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,36 +1,82 @@
 import anime from 'animejs/lib/anime.es.js';
 
-const container = document.querySelector('.container');
+const container = document.querySelector('.tags-list');
 const tags = document.querySelectorAll('.tags-item');
 
-const animateTags = () => {
-  anime({
-    targets: tags,
-    translateX: () => {
-      return anime.random(
-        -container.offsetWidth / 2,
-        container.offsetWidth / 2
-      );
-    },
-    translateY: () => {
-      return anime.random(
-        -container.offsetHeight / 2,
-        container.offsetHeight / 2
-      );
-    },
-    scale: () => {
-      return anime.random(0.5, 1.5);
-    },
-    rotate: () => {
-      return anime.random(-360, 360);
-    },
-    duration: () => {
-      return anime.random(1000, 3000);
-    },
-    easing: 'easeInOutQuad',
-    direction: 'alternate',
-    loop: true,
-  });
-};
+function moveTags() {
+  tags.forEach(tag => {
+    const dx = (Math.random() - 0.5) * 2 * 50;
+    const dy = (Math.random() - 0.5) * 2 * 50;
+    const rotation = (Math.random() - 0.5) * 2 * 360;
 
-animateTags();
+    const tagX = tag.offsetLeft + tag.clientWidth / 2;
+    const tagY = tag.offsetTop + tag.clientHeight / 2;
+
+    const x = tagX + dx;
+    const y = tagY + dy;
+
+    const maxX = container.clientWidth - tag.clientWidth;
+    const maxY = container.clientHeight - tag.clientHeight;
+
+    const constrainedX = Math.min(
+      Math.max(x, tag.clientWidth / 2),
+      maxX + tag.clientWidth / 2
+    );
+    const constrainedY = Math.min(
+      Math.max(y, tag.clientHeight / 2),
+      maxY + tag.clientHeight / 2
+    );
+
+    anime({
+      targets: tag,
+      translateX: constrainedX - tagX,
+      translateY: constrainedY - tagY,
+      rotate: rotation,
+      duration: 3000,
+      easing: 'linear',
+      complete: () => {
+        moveTag(tag); // Call moveTag again for the same tag
+      },
+    });
+  });
+}
+
+function moveTag(tag) {
+  const dx = (Math.random() - 0.5) * 2 * 50;
+  const dy = (Math.random() - 0.5) * 2 * 50;
+  const rotation = (Math.random() - 0.5) * 2 * 360;
+
+  const tagX = tag.offsetLeft + tag.clientWidth / 2;
+  const tagY = tag.offsetTop + tag.clientHeight / 2;
+
+  const x = tagX + dx;
+  const y = tagY + dy;
+
+  const maxX = container.clientWidth - tag.clientWidth;
+  const maxY = container.clientHeight - tag.clientHeight;
+
+  const constrainedX = Math.min(
+    Math.max(x, tag.clientWidth / 2),
+    maxX + tag.clientWidth / 2
+  );
+  const constrainedY = Math.min(
+    Math.max(y, tag.clientHeight / 2),
+    maxY + tag.clientHeight / 2
+  );
+
+  anime({
+    targets: tag,
+    translateX: constrainedX - tagX,
+    translateY: constrainedY - tagY,
+    rotate: rotation,
+    duration: 50000,
+    easing: 'linear',
+    complete: () => {
+      moveTag(tag);
+    },
+  });
+}
+
+tags.forEach(tag => {
+  moveTag(tag);
+});
