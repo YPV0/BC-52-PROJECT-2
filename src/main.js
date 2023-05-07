40ce6ca448474985461f10511fe7d278b202425e
YPV0 <yuriisfw@gmail.com>
2023-05-07 19:44:54 +0300

    conflict resolve

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
 import './js/tags.js';
-<<<<<<< HEAD
-import './js/main-characters.js';
-=======
 import './js/characters-gallery.js';
 import './js/main-header.js';
->>>>>>> 12ff32ae965d2c5fdf895df12e61c78fbb6736fa
