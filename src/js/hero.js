48f85cb0ffd115269073b1ff1b599f56e987b9e3
Slavska <slavskaya.anastasiia.o@gmail.com>
2023-05-10 02:43:36 +0300

    Update hero.js

--- src/js/hero.js
+++ src/js/hero.js
@@ -4,39 +4,35 @@ const footerEl = document.querySelector('#footer');
 const heroEl = document.querySelector('#hero');
 
 let currentSection = document.querySelector('section');
-let nextSection = currentSection.nextElementSibling;
+let nextSection = '';
 let currentNewSection = '';
 
 function callbackFoo() {
   if (currentNewSection.id == 'slider') {
+    footerEl.scrollIntoView({ behavior: 'smooth' });
     currentNewSection = '';
-    nextSection = currentSection.nextElementSibling;
-    nextSection.scrollIntoView({ behavior: 'smooth' });
-    currentNewSection = nextSection;
     return currentNewSection;
   } else if (currentNewSection) {
     nextSection = currentNewSection.nextElementSibling;
     if (nextSection.classList.contains('is-hidden')) {
       nextSection = nextSection.nextElementSibling;
-      nextSection.scrollIntoView({ behavior: 'smooth' });
-    } else {
-      nextSection.scrollIntoView({ behavior: 'smooth' });
+      return nextSection;
     }
+    nextSection.scrollIntoView({ behavior: 'smooth' });
     currentNewSection = nextSection;
     return currentNewSection;
   } else {
-    nextSection.scrollIntoView({ behavior: 'smooth' });
+    nextSection = currentSection.nextElementSibling;
     if (nextSection.classList.contains('is-hidden')) {
       currentNewSection = nextSection.nextElementSibling;
-    } else currentNewSection = nextSection;
+      return currentNewSection;
+    } else {
+      currentNewSection = nextSection.nextElementSibling;
+    }
+    currentNewSection.scrollIntoView({ behavior: 'smooth' });
     return currentNewSection;
   }
 }
-
-btnDown.addEventListener('click', callbackFoo);
-
-const observer = new IntersectionObserver(entry);
-observer.observe(footerEl);
 
 function entry(entries) {
   entries.forEach(entry => {
@@ -54,4 +50,9 @@ const scrollUp = () => {
   heroEl.scrollIntoView({ behavior: 'smooth' });
 };
 
-btnUp.addEventListener('click', scrollUp);
+if (btnDown && btnUp) {
+  btnDown.addEventListener('click', callbackFoo);
+  const observer = new IntersectionObserver(entry);
+  observer.observe(footerEl);
+  btnUp.addEventListener('click', scrollUp);
+}
