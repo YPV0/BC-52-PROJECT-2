50523620692479b683b1cb144d525305e284e3d8
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-05-09 11:09:10 +0200

    error 404 css/ js animation+

--- src/js/main-characters.js
+++ src/js/main-characters.js
@@ -1,33 +1,88 @@
 
+import anime from 'animejs';
+
 const firstImg = document.querySelector('#default-img');
 const mortyImg = document.querySelector('#morty-img');
 const summerImg = document.querySelector('#summer-img');
 const linkEls = document.querySelectorAll('.main-link');
+let currentAnimation = null;
+
+function animateImage(img) {
+  const duration = 200;
+
+  if (currentAnimation) {
+    currentAnimation.pause();
+  }
+
+  const animProperties = {
+    targets: img,
+    opacity: [0, 1],
+    translateY: ['100%', '0%'],
+    easing: 'easeInOutQuad',
+    duration,
+    complete: () => {
+      currentAnimation = null;
+    },
+  };
+
+  currentAnimation = anime(animProperties);
+}
+
+function handleImageTransition(imgId) {
+  const img = document.querySelector(`#${imgId}`);
+
+  if (img.classList.contains('img-hidden')) {
+    const currentImg = document.querySelector(
+      '.img-component:not(.img-hidden)'
+    );
+
+    animateImage(currentImg);
+    currentImg.classList.add('img-hidden');
+
+    animateImage(img);
+    img.classList.remove('img-hidden');
+  }
+}
+
+function handleMouseEnter() {
+  const imgId = this.getAttribute('data-img');
+  handleImageTransition(imgId);
+}
+
+function handleMouseLeave() {
+  const imgId = this.getAttribute('data-img');
+  const img = document.querySelector(`#${imgId}`);
+
+  if (img.classList.contains('img-hidden')) {
+    animateImage(img);
+    img.classList.remove('img-hidden');
+    animateImage(firstImg);
+    firstImg.classList.add('img-hidden');
+  }
+}
+
+function handleTouchStart(event) {
+  const imgId = this.getAttribute('data-img');
+  handleImageTransition(imgId);
+  event.preventDefault();
+}
+
+function handleTouchEnd(event) {
+  const imgId = this.getAttribute('data-img');
+  const img = document.querySelector(`#${imgId}`);
+
+  if (img.classList.contains('img-hidden')) {
+    animateImage(img);
+    img.classList.remove('img-hidden');
+    animateImage(firstImg);
+    firstImg.classList.add('img-hidden');
+  }
+  event.preventDefault();
+}
 
 linkEls.forEach(linkEl => {
-  linkEl.addEventListener('mouseenter', function () {
-    const imgId = this.getAttribute('data-img');
-    const img = document.querySelector(`#${imgId}`);
-
-    firstImg.classList.add('img-hidden', 'img-transition');
-    img.classList.remove('img-hidden', 'img-transition');
-
-    if (img === firstImg) {
-      firstImg.classList.remove('img-hidden', 'img-transition');
-    }
-  });
-
-  linkEl.addEventListener('mouseleave', function () {
-    const imgId = this.getAttribute('data-img');
-    const img = document.querySelector(`#${imgId}`);
-
-    img.classList.add('img-hidden', 'img-transition');
-    firstImg.classList.remove('img-hidden', 'img-transition');
-
-    if (img === firstImg && img === null) {
-      firstImg.classList.remove('img-hidden', 'img-transition');
-    }
-  });
+  linkEl.addEventListener('mouseenter', handleMouseEnter);
+  linkEl.addEventListener('mouseleave', handleMouseLeave);
+  linkEl.addEventListener('touchstart', handleTouchStart);
+  linkEl.addEventListener('touchend', handleTouchEnd);
 });
-
-
