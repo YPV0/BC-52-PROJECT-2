419c9f1e82ef9c0359b336ad21725d1f6f771ea3
YPV0 <yuriisfw@gmail.com>
2023-05-10 23:41:50 +0300

    Update episode-gallery.js

--- src/js/episode-gallery.js
+++ src/js/episode-gallery.js
@@ -8,47 +8,46 @@ const refs = {
   btnLoadMore: document.querySelector('.btn-load-more'),
 };
 
+let seasonImg = '';
+let season = '';
+
+let loadEpisode = await getEpisodes({ page: PAGE });
+
+localStorage.setItem('data', JSON.stringify(loadEpisode.data.results));
+
+const media = window.matchMedia('(min-width: 1440px)');
+let pageSize = media.matches ? 20 : 10;
+let displayedCount = pageSize;
+
 if (refs.gallery && refs.btnLoadMore) {
-  let seasonImg = '';
-  let season = '';
+  refs.gallery.insertAdjacentHTML(
+    'beforeend',
+    onCreateGalleryEpisodes(loadEpisode.data.results.slice(0, pageSize))
+  );
+  refs.btnLoadMore.addEventListener('click', debounce(onLoadMore, 250));
+}
 
-  let loadEpisode = await getEpisodes({ page: PAGE });
-
-  localStorage.setItem('data', JSON.stringify(loadEpisode.data.results));
-
-  const media = window.matchMedia('(min-width: 1440px)');
-  let pageSize = media.matches ? 20 : 10;
-  let displayedCount = pageSize;
-
-  if (refs.gallery && refs.btnLoadMore) {
-    refs.gallery.insertAdjacentHTML(
-      'beforeend',
-      onCreateGalleryEpisodes(loadEpisode.data.results.slice(0, pageSize))
-    );
-    refs.btnLoadMore.addEventListener('click', debounce(onLoadMore, 250));
-  }
-
-  function onCreateGalleryEpisodes(ep) {
-    return ep
-      .map(({ name, air_date, id }) => {
-        if (id < 12) {
-          seasonImg = '/img/season-img/season-1.png';
-          season = '1';
-        } else if (id < 22) {
-          seasonImg = '/img/season-img/season-2.png';
-          season = '2';
-        } else if (id < 32) {
-          seasonImg = '/img/season-img/season-3.png';
-          season = '3';
-        } else if (id < 42) {
-          seasonImg = '/img/season-img/season-4.png';
-          season = '4';
-        } else if (id < 52) {
-          seasonImg = '/img/season-img/season-5.png';
-          season = '5';
-          refs.btnLoadMore.classList.add('is-hidden');
-        }
-        return `
+function onCreateGalleryEpisodes(ep) {
+  return ep
+    .map(({ name, air_date, id }) => {
+      if (id < 12) {
+        seasonImg = '/img/season-img/season-1.png';
+        season = '1';
+      } else if (id < 22) {
+        seasonImg = '/img/season-img/season-2.png';
+        season = '2';
+      } else if (id < 32) {
+        seasonImg = '/img/season-img/season-3.png';
+        season = '3';
+      } else if (id < 42) {
+        seasonImg = '/img/season-img/season-4.png';
+        season = '4';
+      } else if (id < 52) {
+        seasonImg = '/img/season-img/season-5.png';
+        season = '5';
+        refs.btnLoadMore.classList.add('is-hidden');
+      }
+      return `
              <li class="episodes-item">
         <img
           src="${seasonImg}"
@@ -69,143 +68,142 @@ if (refs.gallery && refs.btnLoadMore) {
       </li>
         
         `;
-      })
-      .join('');
-  }
+    })
+    .join('');
+}
 
-  async function onLoadMore() {
-    PAGE += 1;
-    const dataLocal = localStorage.getItem('data');
-    const parserData = dataLocal ? JSON.parse(dataLocal) : [];
+async function onLoadMore() {
+  PAGE += 1;
+  const dataLocal = localStorage.getItem('data');
+  const parserData = dataLocal ? JSON.parse(dataLocal) : [];
 
-    loadEpisode = await getEpisodes({ page: PAGE });
-    pageSize = media.matches ? 20 : 10;
+  loadEpisode = await getEpisodes({ page: PAGE });
+  pageSize = media.matches ? 20 : 10;
 
-    displayedCount = pageSize * PAGE;
-    const start = displayedCount - pageSize;
+  displayedCount = pageSize * PAGE;
+  const start = displayedCount - pageSize;
 
-    if (parserData.length >= start + pageSize) {
-      refs.gallery.insertAdjacentHTML(
-        'beforeend',
-        onCreateGalleryEpisodes(parserData.slice(start, displayedCount))
-      );
-    } else {
-      const newEpisodes = loadEpisode.data.results.slice(0, pageSize);
-      refs.gallery.insertAdjacentHTML(
-        'beforeend',
-        onCreateGalleryEpisodes(newEpisodes)
-      );
-    }
-
-    const newData = parserData.concat(loadEpisode.data.results);
-    localStorage.setItem('data', JSON.stringify(newData));
-  }
-
-  let FILTER = {};
-  let searchTimeout;
-
-  async function onGalleryFilter() {
-    refs.gallery.innerHTML = '';
-    loadEpisode = await getEpisodes(FILTER);
+  if (parserData.length >= start + pageSize) {
     refs.gallery.insertAdjacentHTML(
       'beforeend',
-      onCreateGalleryEpisodes(loadEpisode.data.results)
+      onCreateGalleryEpisodes(parserData.slice(start, displayedCount))
+    );
+  } else {
+    const newEpisodes = loadEpisode.data.results.slice(0, pageSize);
+    refs.gallery.insertAdjacentHTML(
+      'beforeend',
+      onCreateGalleryEpisodes(newEpisodes)
     );
   }
 
-  const dropdownBtn = document.getElementById('all-series-btn');
-  const dropdownMenu = document.getElementById('dropdown');
-  const input = document.getElementById('episode-name-input');
+  const newData = parserData.concat(loadEpisode.data.results);
+  localStorage.setItem('data', JSON.stringify(newData));
+}
 
-  const toggleDropdown = function () {
-    dropdownMenu.classList.toggle('show');
-  };
+let FILTER = {};
+let searchTimeout;
 
-  if (
-    document.getElementById('1-season') &&
-    document.getElementById('2-season') &&
-    document.getElementById('3-season') &&
-    document.getElementById('4-season') &&
-    document.getElementById('5-season') &&
-    dropdownBtn &&
-    dropdownMenu &&
-    document &&
-    input
-  ) {
-    document.addEventListener('click', function (e) {
-      if (!dropdownMenu.contains(e.target)) {
-        dropdownMenu.classList.remove('show');
-      }
-    });
+async function onGalleryFilter() {
+  refs.gallery.innerHTML = '';
+  loadEpisode = await getEpisodes(FILTER);
+  refs.gallery.insertAdjacentHTML(
+    'beforeend',
+    onCreateGalleryEpisodes(loadEpisode.data.results)
+  );
+}
 
-    dropdownBtn.addEventListener('click', function (e) {
-      e.stopPropagation();
-      toggleDropdown();
-    });
+const dropdownBtn = document.getElementById('all-series-btn');
+const dropdownMenu = document.getElementById('dropdown');
+const input = document.getElementById('episode-name-input');
 
-    dropdownMenu.addEventListener('click', function (e) {
-      if (e.target.nodeName !== 'LI') {
-        return;
-      }
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle('show');
+};
 
-      if (e.target.closest('.season-menu') === null) {
-        return;
-      }
+if (
+  document.getElementById('1-season') &&
+  document.getElementById('2-season') &&
+  document.getElementById('3-season') &&
+  document.getElementById('4-season') &&
+  document.getElementById('5-season') &&
+  dropdownBtn &&
+  dropdownMenu &&
+  document &&
+  input
+) {
+  document.addEventListener('click', function (e) {
+    if (!dropdownMenu.contains(e.target)) {
+      dropdownMenu.classList.remove('show');
+    }
+  });
 
+  dropdownBtn.addEventListener('click', function (e) {
+    e.stopPropagation();
+    toggleDropdown();
+  });
+
+  dropdownMenu.addEventListener('click', function (e) {
+    if (e.target.nodeName !== 'LI') {
+      return;
+    }
+
+    if (e.target.closest('.season-menu') === null) {
+      return;
+    }
+
+    onGalleryFilter();
+    toggleDropdown();
+    refs.btnLoadMore.classList.add('is-hidden');
+  });
+  document.getElementById('1-season').addEventListener('click', e => {
+    document
+      .getElementById('1-season')
+      .children[0].classList.toggle('season-menu');
+
+    FILTER = { episode: 's01' };
+    onGalleryFilter();
+  });
+  document.getElementById('2-season').addEventListener('click', () => {
+    document
+      .getElementById('2-season')
+      .children[0].classList.toggle('season-menu');
+
+    FILTER = { episode: 's02' };
+    onGalleryFilter();
+  });
+
+  document.getElementById('3-season').addEventListener('click', () => {
+    document
+      .getElementById('3-season')
+      .children[0].classList.toggle('season-menu');
+
+    FILTER = { episode: 's03' };
+    onGalleryFilter();
+  });
+
+  document.getElementById('4-season').addEventListener('click', () => {
+    document
+      .getElementById('4-season')
+      .children[0].classList.toggle('season-menu');
+
+    FILTER = { episode: 's04' };
+    onGalleryFilter();
+  });
+
+  document.getElementById('5-season').addEventListener('click', () => {
+    document
+      .getElementById('5-season')
+      .children[0].classList.toggle('season-menu');
+
+    FILTER = { episode: 's05' };
+    onGalleryFilter();
+  });
+
+  input.addEventListener('input', function (e) {
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(() => {
+      FILTER = { name: e.target.value };
       onGalleryFilter();
-      toggleDropdown();
-      refs.btnLoadMore.classList.add('is-hidden');
-    });
-    document.getElementById('1-season').addEventListener('click', e => {
-      document
-        .getElementById('1-season')
-        .children[0].classList.toggle('season-menu');
-
-      FILTER = { episode: 's01' };
-      onGalleryFilter();
-    });
-    document.getElementById('2-season').addEventListener('click', () => {
-      document
-        .getElementById('2-season')
-        .children[0].classList.toggle('season-menu');
-
-      FILTER = { episode: 's02' };
-      onGalleryFilter();
-    });
-
-    document.getElementById('3-season').addEventListener('click', () => {
-      document
-        .getElementById('3-season')
-        .children[0].classList.toggle('season-menu');
-
-      FILTER = { episode: 's03' };
-      onGalleryFilter();
-    });
-
-    document.getElementById('4-season').addEventListener('click', () => {
-      document
-        .getElementById('4-season')
-        .children[0].classList.toggle('season-menu');
-
-      FILTER = { episode: 's04' };
-      onGalleryFilter();
-    });
-
-    document.getElementById('5-season').addEventListener('click', () => {
-      document
-        .getElementById('5-season')
-        .children[0].classList.toggle('season-menu');
-
-      FILTER = { episode: 's05' };
-      onGalleryFilter();
-    });
-
-    input.addEventListener('input', function (e) {
-      clearTimeout(searchTimeout);
-      searchTimeout = setTimeout(() => {
-        FILTER = { name: e.target.value };
-        onGalleryFilter();
-      }, 500);
-    });
-  }
+    }, 500);
+  });
 }
