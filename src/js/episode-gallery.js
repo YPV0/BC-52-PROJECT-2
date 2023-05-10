e60b7a063a14c76b11928feb94aea7b9ceba6429
YPV0 <yuriisfw@gmail.com>
2023-05-10 23:30:32 +0300

    Update episode-gallery.js

--- src/js/episode-gallery.js
+++ src/js/episode-gallery.js
@@ -31,19 +31,19 @@ function onCreateGalleryEpisodes(ep) {
   return ep
     .map(({ name, air_date, id }) => {
       if (id < 12) {
-        seasonImg = '/img/season-1.png';
+        seasonImg = './img/season-1.png';
         season = '1';
       } else if (id < 22) {
-        seasonImg = '/img/season-2.png';
+        seasonImg = './img/season-2.png';
         season = '2';
       } else if (id < 32) {
-        seasonImg = '/img/season-3.png';
+        seasonImg = './img/season-3.png';
         season = '3';
       } else if (id < 42) {
-        seasonImg = '/img/season-4.png';
+        seasonImg = './img/season-4.png';
         season = '4';
       } else if (id < 52) {
-        seasonImg = '/img/season-5.png';
+        seasonImg = './img/season-5.png';
         season = '5';
         refs.btnLoadMore.classList.add('is-hidden');
       }
