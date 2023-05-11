b1325017caecc1893a8f2f98eaadeb5b300860d9
YPV0 <yuriisfw@gmail.com>
2023-05-11 15:52:56 +0300

    updated API

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import './js/episode-gallery.js';
 import './js/main-header.js';
 import './js/slider.js';
 import './js/hero.js';
-import './js/main-page-gallery.js';
 import './js/episodes-modal.js';
 import './js/modal-popup-char.js';
